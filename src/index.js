4224e8c032d516c51d4662ebac1b4bf5d490d5cc
Marie-Hermine Schneider <vigneron.mh@gmail.com>
2023-03-06 16:06:04 +0100

    quick refactorisation

--- src/index.js
+++ src/index.js
@@ -1,15 +1,7 @@
 const express = require("express");
-const typeorm = require("typeorm");
-const Wilder = require("./entity/Wilder")
+const dataSource = require("./utils").dataSource;
 
 const app = express();
-
-const dataSource = new typeorm.DataSource({
-  type: "sqlite",
-  database: "./wildersdb.sqlite",
-  synchronize: true,
-  entities: [Wilder],
-});
 
 app.get("/", (req, res) => {
   res.send("Hello World");
@@ -17,7 +9,6 @@ app.get("/", (req, res) => {
 
 const start = async () => {
   await dataSource.initialize();
-  // dataSource.getRepository(Wilder).save({ name: "First Wilder" });
   app.listen(3000, () => console.log("Server stated on 3000"));
 };
 
---
+++ src/utils.js
@@ -0,0 +1,9 @@
+const typeorm = require("typeorm");
+const Wilder = require("./entity/Wilder")
+
+const dataSource = new typeorm.DataSource({
+  type: "sqlite",
+  database: "./wildersdb.sqlite",
+  synchronize: true,
+  entities: [Wilder],
+});
