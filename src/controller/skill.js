3da13e0867639d52dd5cf8f23a7a078f1da372f7
Marie-Hermine Schneider <vigneron.mh@gmail.com>
2023-03-07 10:38:16 +0100

    change then/catch with async/await and try/catch

--- src/controller/skill.js
+++ src/controller/skill.js
@@ -2,48 +2,54 @@ const dataSource = require("../utils").dataSource;
 const Skill = require("../entity/Skill");
 
 module.exports = {
-  create: (req, res) => {
-    dataSource
-      .getRepository(Skill)
-      .save(req.body)
-      .then(() => {
-        res.send("Created skill");
-      })
-      .catch(() => {
-        res.send("Error while creating skill")
-      })
+  create: async (req, res) => {
+    try {
+      const skillToCreate = await dataSource
+        .getRepository(Skill)
+        .save(req.body)
+        .then(() => {
+          res.send("Created skill");
+        })
+    } catch (err) {
+      console.log(err);
+      res.send("Error while creating skill")
+    }
   },
-  read: (req, res) => {
-    dataSource
-      .getRepository(Skill)
-      .find()
-      .then((data) => {
-        res.send(data);
-      })
-      .catch(() => {
-        res.send("Error while reading skills")
-      })
+  read: async (req, res) => {
+    try {
+      const skillToRead = await dataSource
+        .getRepository(Skill)
+        .find()
+        .then((data) => {
+          res.send(data);
+        })
+    } catch (err) {
+      console.log(err);
+      res.send("Error while reading skills")
+    }
   },
-  update: (req, res) => {
-    dataSource
-      .getRepository(Skill)
-      .update(req.params.id, { name: req.body.name })
-      .then(() => {
-        res.send("Updated skill");
-      })
-      .catch(() => {
-        res.send("Error while updating skill")
-      })
+  update: async (req, res) => {
+    try {
+      const skillToUpdate = await dataSource
+        .getRepository(Skill)
+        .update(req.params.id, { name: req.body.name })
+        .then(() => {
+          res.send("Updated skill");
+        })
+    } catch (err) {
+      res.send("Error while updating skill")
+    }
   },
-  delete: (req, res) => {
-    dataSource
-      .getRepository(Skill)
-      .delete(parseInt(req.params.id))
-      .then(() => {
-        res.send("Deleted skill");
-      })
-      .catch(() => {
-        res.send("Error while deleting skill")
-      })
+  delete: async (req, res) => {
+    try {
+      const skillToDelete = await dataSource
+        .getRepository(Skill)
+        .delete(parseInt(req.params.id))
+        .then(() => {
+          res.send("Deleted skill");
+        })
+    } catch (err) {
+      res.send("Error while deleting skill")
+    }
   },
 }
